30fcec91ac1a0dc978bb85c441bd09e89e91a728
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2023-10-08 23:26:59 -0300

    lobby

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import {LobbyRoom, Server} from "colyseus";
 import { createServer } from "http";
 import express from "express";
+import {WebSocketTransport} from "@colyseus/ws-transport";
 const port = Number(process.env.port) || 3000;
 
 const app = express();
 app.use(express.json());
+app.use('/', express.static('static'));
 
 const gameServer = new Server({
-    server: createServer(app)
+    transport: new WebSocketTransport({server: createServer(app)})
 });
 
 gameServer.define("lobby", LobbyRoom);
