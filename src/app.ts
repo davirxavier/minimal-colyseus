3745ef4b5a8bbaa007beba356a990cd468ce9778
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2023-10-08 23:49:04 -0300

    changes

--- src/app.ts
+++ src/app.ts
@@ -12,5 +12,10 @@ const gameServer = new Server({
     transport: new WebSocketTransport({server: createServer(app)})
 });
 
+app.use('*', (req, res) => {
+    console.log(`Not found: ${req.baseUrl}`);
+    res.sendStatus(404);
+});
+
 gameServer.define("lobby", LobbyRoom);
 gameServer.listen(port).then();
