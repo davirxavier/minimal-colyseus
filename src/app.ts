c63fafb21b6ad8e8bca8485eaa7d59abf6434403
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2023-10-12 13:09:27 -0300

    fix nginx

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 import {LobbyRoom, Server} from "colyseus";
-import { createServer } from "http";
+import expressify from 'uwebsockets-express';
+import {uWebSocketsTransport} from "@colyseus/uwebsockets-transport";
 import express from "express";
-import {WebSocketTransport} from "@colyseus/ws-transport";
 const port = Number(process.env.port) || 3000;
 
-const app = express();
-app.use(express.json());
-app.use(express.static('static'));
-
+const transport = new uWebSocketsTransport();
 const gameServer = new Server({
-    transport: new WebSocketTransport({server: createServer(app)})
+    transport: transport
 });
+
+const app = expressify(transport.app);
+app.use(express.json());
+app.use('/', express.static('static'));
 
 app.use('*', (req, res) => {
     console.log(`Not found: ${req.baseUrl}`);
